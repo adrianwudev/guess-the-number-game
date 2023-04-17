8e4d413c97e1b58a5c0caf867bce79cce0e38b9c
adrian <adrian657984@gmail.com>
2023-04-17 08:52:38 +0800

    add lombok with jar file

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -2,17 +2,15 @@ package academy.learnprogramming.console;
 
 import academy.learnprogramming.MessageGenerator;
 import academy.learnprogramming.config.GameConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+@Slf4j
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Guess The Number Game");
+        log.info("Guess The Number Game");
 
         // create context (container)
         ConfigurableApplicationContext context
@@ -20,8 +18,8 @@ public class Main {
 
         // get messageGenerator bean from context (container)
         MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-        logger.info("getMainMessage = {}", messageGenerator.getMainMessage());
-        logger.info("getResultMessage = {}", messageGenerator.getResultMessage());
+        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
+        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
 
         // close context (container)
         context.close();
