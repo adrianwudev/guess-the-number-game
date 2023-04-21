385f128aaee0f68c5612bcbaae558e800317a909
adrian <adrian657984@gmail.com>
2023-04-21 10:15:54 +0800

    Add Thymeleaf Internationalization

--- web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,14 +1,23 @@
 package academy.learnprogramming.config;
 
 import academy.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new FixedLocaleResolver(new Locale("es", "ES"));
     }
 }
